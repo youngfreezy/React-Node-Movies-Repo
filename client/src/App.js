75b41c645e064868b1d148470eed056cadfcd0e4
Fareez Ahmed <fareezahmed@Fareezs-MacBook-Pro.local>
2017-05-21 14:39:10 -0400

    individual view shell

--- client/src/App.js
+++ client/src/App.js
@@ -16,11 +16,21 @@ class App extends Component {
         return this.setState({movies: newMovies});
     })
   }
+  // TODO: refactor into List + List-Item views.
+  showIndividualView = (e, movie) => {
+    this.individualView = true;
+    axios.get('/movies/' + movie.id, {params: {
+      id: movie.id
+    }})
+    .then(movie => {
+        console.log(movie, 'the movie returned from showIndividualView')
+        return this.setState({movies: [movie.data]});
+    })
+  }
 
-  showIndividualView() {console.log('getting here')}
- 
-  componentDidMount() {
+  backToSummaryView = () => {
     this.pageNumber = 1;
+    this.individualView = false;
     axios.get('/movies', {params: {
       page: this.pageNumber
     }})
@@ -29,16 +39,28 @@ class App extends Component {
         return this.setState({movies: movies.data.results});
     })
   }
+ 
+  componentDidMount() {
+    this.backToSummaryView()
+  }
   render() {
     return (
       <div className="App">
         <h1>movies</h1>
         <button onClick={this.loadMore.bind(this)}>Load More </button>
+        <button onClick={this.backToSummaryView.bind(this)}> Back To Summary</button>
         <div className="grid">
           {this.state.movies.map(movie =>
             <div key={movie.id} className="grid-item">
               <div>{movie.title}</div>
-              <img onClick={this.showIndividualView.bind(this)} alt="movie" src={'http://image.tmdb.org/t/p/w185/' + movie.poster_path}></img>
+              {this.individualView &&
+                <div>
+                <div><b>overview: </b>{movie.overview}</div>
+              <div><b>tagline:</b>{movie.tagline}</div>
+              <div><b>status: </b>{movie.status}</div>
+              </div>
+            }
+              {!movie.notFound && <img onClick={(e) => this.showIndividualView(e, movie)} alt="movie" src={'http://image.tmdb.org/t/p/w185/' + movie.poster_path}></img>}
             </div>
           )}
         </div>
--- routes/movies.js
+++ routes/movies.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+//wouldn't keep this here in the real world
 const apiKey = '9952def8e4e171a89ef202376a61cbe6';
 router.get('/', function(req, res, next) {
-	console.log('the actual request', req);
-    request('http://api.themoviedb.org/3/discover/movie?language=en&sort_by=popularity.asc&api_key=' + apiKey + '&page=' + req.query.page, (error, response, body) => {
+    request('http://api.themoviedb.org/3/discover/movie?language=en&sort_by=popularity.asc&primary_release_year=2017&api_key=' + apiKey + '&page=' + req.query.page, (error, response, body) => {
         if (!error && response.statusCode == 200) {
         	res.send(body)
         }
     })
-
 });
+router.get('/:id', function (req, res, next) {
+	let id = req.params.id;
+	request('http://api.themoviedb.org/3/movie/' + req.params.id + '?api_key=' + apiKey, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+        	console.log('the individual movie', body)
+        	res.send(body)
+        }
 
+        if (response.statusCode === 404) {
+        	res.json({
+        		title: "Summary not found",
+        		overview: "The Movies DB does not have an individual view for this movie",
+        		tagline: "Sorry!",
+        		status: "N/A",
+        		notFound: true
+        	})
+        }
+
+    })
+
+})
 module.exports = router;
